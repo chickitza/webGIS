70fd9c293730bc258f15c7729f70deda51860a27
chickitza <1771751822@qq.com>
2023-04-28 22:43:54 +0800

    实现底图

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function getURLParameter(name) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
+var coordinateFormat = function (coordinate) {
+    var hdms = ol.coordinate.toStringHDMS(coordinate); // 地理坐标
+    return hdms;
+  };
