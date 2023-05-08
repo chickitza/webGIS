4336e34d5b3cdff4c433e45f7a8c3e18419f0708
chickitza <1771751822@qq.com>
2023-05-08 21:40:47 +0800

    popUP & Length & Area

--- script.js
+++ script.js
@@ -5,3 +5,89 @@ var coordinateFormat = function (coordinate) {
     var hdms = ol.coordinate.toStringHDMS(coordinate); // 地理坐标
     return hdms;
   };
+
+function lengthMeasure(){
+    this.setToolbarDisactive();
+    this.$refs.length.classList.add("active");
+    iSelect.setActive(false);
+    measure.measure(this.map,'LineString');
+
+}
+
+var measureTooltipElement;
+
+const formatLength = function (line) {
+    const length = ol.sphere.getLength(line);
+    var output;
+    if (length > 100) {
+      output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
+    } else {
+      output = Math.round(length * 100) / 100 + ' ' + 'm';
+    }
+    return output;
+};
+
+const formatArea = function (polygon) {
+    const area = ol.sphere.getArea(polygon);
+    var output;
+    if (area > 10000) {
+      output = Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
+    } else {
+      output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
+    }
+    return output;
+};
+
+function DrawMeasure() {
+    createMeasureTooltip();
+    var listener, sketch;
+    draw.on('drawstart', function (evt) {
+      // set sketch
+      sketch = evt.feature;
+  
+      /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
+      var tooltipCoord = evt.coordinate;
+  
+      listener = sketch.getGeometry().on('change', function (evt) {
+        const geom = evt.target;
+        var output;
+        if (geom instanceof Polygon) {
+          output = formatArea(geom);
+          tooltipCoord = geom.getInteriorPoint().getCoordinates();
+        } else if (geom instanceof LineString) {
+          output = formatLength(geom);
+          tooltipCoord = geom.getLastCoordinate();
+        }
+        measureTooltipElement.innerHTML = output;
+        measureTooltip.setPosition(tooltipCoord);
+      });
+    });
+  
+    draw.on('drawend', function () {
+      measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
+      measureTooltip.setOffset([0, -7]);
+      // unset sketch
+      sketch = null;
+      // unset tooltip so that a new one can be created
+      measureTooltipElement = null;
+      createMeasureTooltip();
+      unByKey(listener);
+    });
+}
+
+function createMeasureTooltip() {
+    if (measureTooltipElement) {
+      measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+    }
+    measureTooltipElement = document.createElement('div');
+    measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+    measureTooltipElement.id = 'ol-tooltip ol-tooltip-measure';
+    measureTooltip = new ol.Overlay({
+      element: measureTooltipElement,
+      offset: [0, -15],
+      positioning: 'bottom-center',
+      stopEvent: false,
+      insertFirst: false,
+    });
+    map.addOverlay(measureTooltip);
+}
